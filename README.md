62023c741481ce08d04aef55bced9f8dd7641f64
= <edwinsilvestre0101@gmail.com>
2022-07-17 18:47:59 -0400

    He agregado una breve descripcion de mi projecto http

---
+++ README.md
@@ -0,0 +1,9 @@
+# Servidor local http (hypertext transfer protocol)
+--- Code open source ----
+he creado un simple servidor http
+con las funcionalidades de detectar un
+contexto que no existe devolviendo la pagina con el error.
+se seguira avanzando con el.
+
+ejecuta:
+$ Node http.js
--- http.js
+++ http.js
@@ -4,6 +4,6 @@ import {webserver} from "./sources/ws.js";
 
 const server = http.createServer();
 
-server.listen(2000, "127.80.70.5", ()=> console.log("Running Server"));
+server.listen(2000, "127.80.70.5", ()=> console.log("Running Server... http://127.80.70.5:2000"));
 
 server.on("request", webserver);
