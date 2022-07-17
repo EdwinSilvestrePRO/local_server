12196122b6898539248e4fa2e80cd85b0083d043
= <edwinsilvestre0101@gmail.com>
2022-07-17 17:26:42 -0400

    I test http Server with home

--- sources/ws.js
+++ sources/ws.js
@@ -1,3 +1,11 @@
+import {createReadStream} from 'fs';
 export function webserver(request, response) {
-    
+    const home = createReadStream("./home.html")
+    console.log(request.url);
+    response.statusCode = 200;
+    response.setHeader("content-type", "text/html");
+    home.pipe(response);
 }
+
+
+// http://127.80.70.5:2000/
